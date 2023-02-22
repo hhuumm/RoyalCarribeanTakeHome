5c615ead6af432500469f2d90c1ea6f77dc0f8cc
hhuumm <h4mid@hotmail.com>
2023-02-22 14:30:40 -0500

    Form and Endpoint for Adding Employee, no save yet

--- RoyalCarribeanTakeHome/RoyalCarribeanTakeHome/views.py
+++ RoyalCarribeanTakeHome/RoyalCarribeanTakeHome/views.py
@@ -3,7 +3,7 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template,request
 from RoyalCarribeanTakeHome import app
 
 @app.route('/')
@@ -13,25 +13,21 @@ def home():
     return render_template(
         'index.html',
         title='Home Page',
-        year=datetime.now().year,
+        current_date = datetime.today().strftime('%Y-%m-%d'),
     )
 
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
+@app.route('/submit-employee', methods=['POST'])
+def submit_employee():
+    # Get the form data from the request object
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    email_address = request.form.get('email_address')
+    hire_date = request.form.get('hire_date')
+    job_title = request.form.get('job_title')
+    agency_num = request.form.get('agency_num')
+    registration_date = request.form.get('registration_date')
 
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
+    # Process the form data as needed (e.g. store in a database)
+    # ...
+
+    return 'Employee submitted successfully'
