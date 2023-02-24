1115a7d3fb7c93b1dc200daa0498d8b528e10cc2
hhuumm <h4mid@hotmail.com>
2023-02-23 22:10:57 -0500

    Adding testimonial, almost ready for deployment

---
+++ RoyalCarribeanTakeHome/ReadMe.txt
@@ -0,0 +1,34 @@
+Royal Caribbean Employee List
+This is a repository for the Caribbean Cruise FullStack Software Engineer Role. The project is designed to do demonstrate full CRUD functionality and match the criteria requested by the hiring managers.
+
+Technologies
+The project is built using the following technologies:
+
+HTML
+CSS
+JavaScript
+Bootstrap
+Python
+Flask
+MySQL
+
+Installation
+To run the project locally, you will need to:
+
+Clone this repository to your local machine
+Install the required packages by running pip install -r requirements.txt
+Set up the database by running python setup_database.py
+Start the Flask server by running python app.py
+Usage
+To use the project, open a web browser and go to http://localhost:5000/. From there, you can do X, Y, and Z.
+
+Contributing
+If you would like to contribute to the project, please follow these steps:
+
+Fork the repository
+Create a new branch for your changes
+Make your changes and commit them
+Push your changes to your forked repository
+Create a pull request from your branch to the main repository
+License
+This project is licensed under the MIT License.
--- RoyalCarribeanTakeHome/RoyalCarribeanTakeHome/views.py
+++ RoyalCarribeanTakeHome/RoyalCarribeanTakeHome/views.py
@@ -67,7 +67,7 @@ def submit_employee():
         'index.html',
         title='Home Page',
         current_date = datetime.today().strftime('%Y-%m-%d'),
-        error=f"Error: person ID {person_id} is already in use"
+        error=f"Error: person ID is already in use"
         )
 
     # Insert the employee data into the Employees table
@@ -79,7 +79,7 @@ def submit_employee():
     values = (person_id, first_name, last_name, email_address, hire_date, job_title, agency_num, registration_date)
     cursor.execute(query, values)
     cnx.commit()
-    return redirect(url_for('home', success="you have successfully submitted an employee"))
+    return redirect(url_for('home', success="You have successfully submitted an employee"))
 
 @app.route('/api/employees', methods=['GET'])
 def get_employees():
@@ -110,7 +110,7 @@ def delete_employee(id):
     query = "DELETE FROM Employees WHERE person_id = %s"
     cursor.execute(query, (id,))
     cnx.commit()
-    return redirect(url_for('home', delete_success="you have successfully deleted employee " + id))
+    return redirect(url_for('home', delete_success="You have successfully deleted employee."))
 
 @app.route('/edit/<person_id>', methods=['GET', 'POST'])
 def edit_employee(person_id):
